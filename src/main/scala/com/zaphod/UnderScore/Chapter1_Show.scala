02731df709159d222676eab496821c538698b217
Norbert Scheller <norbert@riskident.com>
2018-05-16 15:58:15 +0200

    underscore - chapter 1

---
+++ src/main/scala/com/zaphod/UnderScore/Chapter1_Show.scala
@@ -0,0 +1,20 @@
+package com.zaphod.UnderScore
+
+object Chapter1_Show extends App {
+  import cats.Show
+  import cats.instances.int._
+  import cats.instances.string._
+  import cats.syntax.show._
+
+  final case class Cat(name: String, age: Int, color: String)
+
+  implicit val catShow = Show.show[Cat] { cat =>
+    val n = cat.name.show
+    val a = cat.age.show
+    val c = cat.color.show
+
+    s"$n is a $a year-old $c cat"
+  }
+
+  println(Cat("Garfield", 38, "ginger and black").show)
+}
