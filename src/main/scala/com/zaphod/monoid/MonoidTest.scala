4ca53f9ce0a98e15a5b2b492210fc14f75748f65
Norbert Scheller <norbert@riskident.com>
2018-10-11 09:22:41 +0200

    master - added BoxMonoidTest

--- src/main/scala/com/zaphod/monoid/MonoidTest.scala
+++ src/main/scala/com/zaphod/monoid/MonoidTest.scala
@@ -31,3 +31,60 @@ object MonoidTest extends App {
   println(s"li = $li")
   println(s"ls = $ls")
 }
+
+object BoxMonoidTest extends App {
+  import cats.instances.int._
+  import cats.instances.string._
+
+  case class Box[T](t: T)
+
+  val bi1 = Box(1)
+  val bi2 = Box(2)
+
+  val bs1 = Box("s1")
+  val bs2 = Box("s2")
+
+  object WithMonoid {
+    import cats.Monoid
+    import cats.syntax.semigroup._
+
+    implicit def boxMonoid[T: Monoid]: Monoid[Box[T]] = new Monoid[Box[T]] {
+      override def empty: Box[T] = Box(Monoid[T].empty)
+
+      override def combine(x: Box[T], y: Box[T]): Box[T] = Box(Monoid[T].combine(x.t, y.t))
+    }
+
+    val bi3 = bi1 |+| bi2
+    val bi4 = List(bi1, bi2, bi3).combineAll
+
+    println(s"$bi1 + $bi2 = $bi3 | $bi4")
+
+    val bs3 = bs1 |+| bs2
+    val bs4 = List(bs1, bs2, bs3).combineAll
+
+    println(s"$bs1 + $bs2 = $bs3 | $bs4")
+  }
+
+  object WithApplicative {
+    import cats.Applicative
+
+    implicit val boxApplicative = new Applicative[Box] {
+      override def pure[A](a: A): Box[A] = Box(a)
+
+      override def ap[A, B](ff: Box[A => B])(fa: Box[A]): Box[B] = {
+        val a = fa.t
+        val f = ff.t
+
+        Box(f(a))
+      }
+    }
+
+    val bi5 = Applicative[Box].map2(bi1, bi2)(Monoid[Int].combine)
+    val bs5 = Applicative[Box].map2(bs1, bs2)(Monoid[String].combine)
+
+    println(s"$bi5, $bs5")
+  }
+
+  WithMonoid
+  WithApplicative
+}
