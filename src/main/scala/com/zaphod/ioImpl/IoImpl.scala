44062f4c76816c0c1131139e670878c190137bc0
Norbert Scheller <norbert@riskident.com>
2020-04-27 18:32:57 +0200

    IoImpl - 1st runtime

--- src/main/scala/com/zaphod/ioImpl/IoImpl.scala
+++ src/main/scala/com/zaphod/ioImpl/IoImpl.scala
@@ -22,10 +22,38 @@ case class Pure[+A](v: A) extends IO[A]
 case class Delay[+A](eff: () => A) extends IO[A]
 
 object IO {
+  implicit class RichList[A](l: List[A]) {
+    def push(a: => A): List[A]    = a :: l
+    def pop: Option[(A, List[A])] =
+      if (l.isEmpty)
+        Option.empty[(A, List[A])]
+      else
+        Option(l.head, l.tail)
+  }
+
   def apply[A](a: => A): IO[A] = Delay( () => a )
 
   def pure[A](a: A): IO[A] = Pure(a)
   def flatMap[A,B](fa: IO[A])(f: A => IO[B]): IO[B] = FlatMap(fa, f)
+
+  def unsafeRunSync[A](io: IO[A]): A = {
+    def loop(current: IO[Any], stack: List[Any => IO[Any]]): A = current match {
+      case FlatMap(io, k) =>
+        loop(io, stack.push(k))
+      case Delay(body) =>
+        val res = body()
+        loop(pure(res), stack)
+      case Pure(v) =>
+        stack.pop match {
+          case None                => v.asInstanceOf[A]
+          case Some((bind, stack)) =>
+            val nextIO = bind(v)
+            loop(nextIO, stack)
+        }
+    }
+
+    loop(io, List.empty)
+  }
 }
 
 object IoImpl extends App {
@@ -35,4 +63,6 @@ object IoImpl extends App {
   def hello = IO.flatMap(prompt)(n => put(s"Hello $n!"))
 
   println(s"hello: $hello")
+
+  IO.unsafeRunSync(hello)
 }
