daf9c7a4533d3a9b0ef22f1eaed3fc3efb51a851
Norbert Scheller <norbert@riskident.com>
2021-11-09 17:56:32 +0100

    master - JobScheduler fake Zzz impl

--- src/main/scala/com/zaphod/essentialEffects/jobscheduler/JobScheduler.scala
+++ src/main/scala/com/zaphod/essentialEffects/jobscheduler/JobScheduler.scala
@@ -3,9 +3,9 @@ package com.zaphod.essentialEffects.jobscheduler
 import cats.data.Chain
 import cats.effect.{ExitCode, IO, IOApp, Outcome, Ref}
 import cats.effect.kernel.Resource.ExitCase
-import cats.effect.kernel.{Deferred, Fiber}
+import cats.effect.kernel.{Deferred, Fiber, Resource}
 import cats.effect.std.Semaphore
-import cats.implicits.{toFlatMapOps, toTraverseOps}
+import cats.implicits.{toFlatMapOps, toFunctorOps, toTraverseOps}
 import com.zaphod.util.Debug.DebugHelper
 
 import java.util.UUID
@@ -46,6 +46,7 @@ trait JobScheduler {
 }
 
 object JobScheduler {
+
   case class State(
                     maxRunning: Int,
                     scheduled: Chain[Job.Scheduled] = Chain.empty,
@@ -67,34 +68,49 @@ object JobScheduler {
       copy(running = running.removed(job.id),  completed = completed :+ job)
   }
 
-  def scheduler(stateRef: Ref[IO, State]): JobScheduler =
+  def scheduler(stateRef: Ref[IO, State], zzz: Zzz): JobScheduler =
     new JobScheduler {
-      override def schedule(task: IO[_]): IO[Job.Id] =
+      override def schedule(task: IO[_]): IO[Job.Id] = {
         for {
           job <- Job.create(task)
           _   <- IO(job).debug
           _   <- stateRef.update(_.enqueue(job))
           _   <- stateRef.get.debug
+          _   <- zzz.wakeUp
         } yield job.id
+      }
     }
 
-  def create(max: Int): IO[JobScheduler] =
+  def resource(max: Int): IO[Resource[IO, JobScheduler]] =
     for {
-      stateRef <- Ref[IO].of(State(max, Chain.empty, Map.empty, Chain.empty))
-    } yield scheduler(stateRef)
+      state <- Ref[IO].of(State(max))
+      zzz   <- Zzz.apply
+      scheduler = new JobScheduler {
+        override def schedule(task: IO[_]): IO[Job.Id] =
+          for {
+            job <- Job.create(task)
+            _   <- state.update(_.enqueue(job))
+            _   <- zzz.wakeUp
+          } yield job.id
+      }
+      reactor = Reactor(state)
+      onStart = (id: Job.Id) => IO.unit
+      onComplete = (id: Job.Id, exitCase: ExitCase) => zzz.wakeUp
+      loop = (zzz.sleep *> reactor.whenAwake(onStart, onComplete)).foreverM
+    } yield loop.background.as(scheduler)
 }
 
 trait Reactor {
   def whenAwake(
-               onStart: Job.Id => IO[Unit],
-               addComplete: (Job.Id, ExitCase) => IO[Unit]
+                 onStart: Job.Id => IO[Unit],
+                 onComplete: (Job.Id, ExitCase) => IO[Unit]
                ): IO[Unit]
 }
 
 object Reactor {
   def apply(stateRef: Ref[IO, JobScheduler.State]): Reactor =
     new Reactor {
-      override def whenAwake(onStart: Job.Id => IO[Unit], addComplete: (Job.Id, ExitCase) => IO[Unit]): IO[Unit] = {
+      override def whenAwake(onStart: Job.Id => IO[Unit], onComplete: (Job.Id, ExitCase) => IO[Unit]): IO[Unit] = {
         def startNextJob: IO[Option[Job.Running]] =
           for {
             job     <- stateRef.modify(_.dequeue)
@@ -116,7 +132,7 @@ object Reactor {
 
         def jobCompleted(job: Job.Completed): IO[Unit] =
           stateRef.update(_.onComplete(job))
-            .flatTap(_ => addComplete(job.id, job.exitCase))
+            .flatTap(_ => onComplete(job.id, job.exitCase))
 
         startNextJob.iterateUntil(_.isEmpty).void
       }
@@ -125,7 +141,7 @@ object Reactor {
 
 trait Zzz {
   def sleep: IO[Unit]
-  def awake: IO[Unit]
+  def wakeUp: IO[Unit]
 }
 
 object Zzz {
@@ -142,7 +158,7 @@ object Zzz {
       case Awake  => Asleep -> semaphore.acquire
     }
 
-    override def awake: IO[Unit] = state.modify {
+    override def wakeUp: IO[Unit] = state.modify {
       case Asleep => Awake -> semaphore.release
       case Awake  => Awake -> IO.unit
     }
@@ -154,12 +170,22 @@ object Zzz {
 object RunScheduler extends IOApp {
   override def run(args: List[String]): IO[ExitCode] =
     for {
-      scheduler <- JobScheduler.create(4)
-      jobId     <- scheduler.schedule(IO("whee").debug)
-      _         <- IO(s"jobId: $jobId").debug
-      fiber     <- IO.sleep(1000.millis).start
-      _         <- IO("waiting...").debug
-      _         <- fiber.join
+      resource  <- JobScheduler.resource(2)
+      _         <- resource.use { scheduler =>
+        for {
+          jobId1 <- scheduler.schedule(IO("whee1").debug)
+          jobId2 <- scheduler.schedule(IO("whee2").debug)
+          jobId3 <- scheduler.schedule(IO("whee3").debug)
+          jobId4 <- scheduler.schedule(IO("whee4").debug)
+//          _ <- IO(s"jobId: $jobId1").debug
+//          _ <- IO(s"jobId: $jobId2").debug
+//          _ <- IO(s"jobId: $jobId3").debug
+//          _ <- IO(s"jobId: $jobId4").debug
+          fiber <- IO.sleep(100.millis).start
+          _ <- IO("waiting...").debug
+          _ <- fiber.join
+        } yield ()
+      }
     } yield ExitCode.Success
 
 }
