2f22905148c274872f0f3eb644139349b97d84c3
Norbert Scheller <norbert.scheller@gmail.com>
2022-10-03 19:22:37 +0200

    master - MethodEnhancement more generic

--- src/main/scala/com/zaphod/stuff/MethodEnhancementTest.scala
+++ src/main/scala/com/zaphod/stuff/MethodEnhancementTest.scala
@@ -2,14 +2,24 @@ package com.zaphod.stuff
 
 object MethodEnhancementTest extends App {
 
-  implicit class OptionOpsInt(i: Int) {
-    def some: Option[Int] = Option[Int](i)
-    def none: Option[Int]  = Option.empty[Int]  // anyways this makes no sense
+//  implicit class OptionOpsInt(i: Int) {
+//    def some: Option[Int] = Option[Int](i)
+//    def none: Option[Int] = Option.empty[Int]  // anyways this makes no sense
+//  }
+
+  implicit class OptionOps[A](a: A) {
+    def some: Option[A] = Option[A](a)
+    def none: Option[A] = Option.empty[A]
   }
 
   val o1 = 1.some
   val n1 = 1.none
 
+  val o2 = 'a'.some
+  val n2 = 'a'.none
+
   println(s"o1: $o1")
   println(s"n1: $n1")
+  println(s"o2: $o2")
+  println(s"n2: $n2")
 }
